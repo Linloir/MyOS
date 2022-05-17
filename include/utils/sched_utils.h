5cb9fa853041d38f174bf4857fad88e38ec0d5f4
Linloir <3145078758@qq.com>
2022-05-17 15:17:03 +0800

    minor fix

--- include/utils/sched_utils.h
+++ include/utils/sched_utils.h
@@ -1,13 +1,13 @@
 /*** 
  * Author       : Linloir
  * Date         : 2022-05-17 14:17:32
- * LastEditTime : 2022-05-17 14:40:14
+ * LastEditTime : 2022-05-17 15:16:54
  * Description  : 
  */
 
 #ifndef _SCHED_UTILS_H_
 #define _SCHED_UTILS_H_
 
-extern void asm_switch_thread(uint32 cur, uint32 next);
+extern "C" void asm_switch_thread(uint32 cur, uint32 next);
 
 #endif
