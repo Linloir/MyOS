3aed39c684ad0bf2f8ecd6827aff3e6f0b6ae9a2
Linloir <3145078758@qq.com>
2022-05-31 21:11:55 +0800

    add kernelloader

--- src/boot/bootloader.asm
+++ src/boot/bootloader.asm
@@ -77,68 +77,6 @@ jmp CODE_SELECTOR:KERNEL_START_ADDR
 ; Halt
 jmp $
 
-; asm_read_hard_disk:                           
-;     push ebp
-;     mov ebp, esp
-
-;     push eax
-;     push ebx
-;     push ecx
-;     push edx
-
-;     mov eax, [ebp + 4 * 3] ; 逻辑扇区低16位
-
-;     mov edx, 0x1f3
-;     out dx, al    ; LBA地址7~0
-
-;     inc edx        ; 0x1f4
-;     mov al, ah
-;     out dx, al    ; LBA地址15~8
-
-;     xor eax, eax
-;     inc edx        ; 0x1f5
-;     out dx, al    ; LBA地址23~16 = 0
-
-;     inc edx        ; 0x1f6
-;     mov al, ah
-;     and al, 0x0f
-;     or al, 0xe0   ; LBA地址27~24 = 0
-;     out dx, al
-
-;     mov edx, 0x1f2
-;     mov al, 1
-;     out dx, al   ; 读取1个扇区
-
-;     mov edx, 0x1f7    ; 0x1f7
-;     mov al, 0x20     ;读命令
-;     out dx,al
-
-;     ; 等待处理其他操作
-;   .waits:
-;     in al, dx        ; dx = 0x1f7
-;     and al,0x88
-;     cmp al,0x08
-;     jnz .waits                         
-    
-
-;     ; 读取512字节到地址ds:bx
-;     mov ebx, [ebp + 4 * 2]
-;     mov ecx, 256   ; 每次读取一个字，2个字节，因此读取256次即可          
-;     mov edx, 0x1f0
-;   .readw:
-;     in ax, dx
-;     mov [ebx], eax
-;     add ebx, 2
-;     loop .readw
-      
-;     pop edx
-;     pop ecx
-;     pop ebx
-;     pop eax
-;     pop ebp
-
-;     ret
-
 asm_read_sector_pm:
     ; push readToAddr
     ; push sector
